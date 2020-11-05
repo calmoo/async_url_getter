bbcd977a21ebca46f4a2e988bb515ebb4aea8e08
Noah Skelton <skelton.noah@gmail.com>
2020-11-05 18:35:43 +0000

    Added limit for concurrent connections

---
+++ TODO.md
@@ -0,0 +1,5 @@
+* Add tests
+* Make the request time recording accurate
+* Fill in the README
+* Make this a CLI - use click
+* Calculate metrics
--- main.py
+++ main.py
@@ -1,56 +1,81 @@
 import asyncio
 import aiohttp
 from asyncio.exceptions import TimeoutError
+from aiohttp.client_exceptions import InvalidURL, ClientConnectorError, ServerDisconnectedError, ClientError
 import time
+from socket import gaierror
 
 
-class RequestStats:
+class RequestInfo:
     """
     Details of a process.
     Total time is separate from start time and end time because there could be
     system clock changes while running the process.
     """
     def __init__(
-        self, url: str, start_time: float, end_time: float, total_time: float,
-            response_status: int
+        self, url: str, total_time: float, status_code: int
     ):
         self.url = url
-        self.response_status = response_status
+        self.status_code = status_code
         self.total_time = total_time
 
-async def get(session: aiohttp.ClientSession, url: str) -> RequestStats:
+    def __repr__(self):
+        results = f"Request to {self.url} took "
+        return f"{self.url} {self.status_code} {self.total_time}"
 
+async def get(session: aiohttp.ClientSession, url: str) -> RequestInfo:
+    """
+    Raises:
+        ...
+        ...
+    """
+    start_time_monotonic = time.monotonic()
+    print(time.time())
     try:
-        async with session.request('GET', url=url, timeout=30) as response:
-            start_time_monotonic = time.monotonic()
-            await response.text(errors="replace")
-            end_time_monotonic = time.monotonic()
-            total_time = end_time_monotonic - start_time_monotonic
-            request_stats = RequestStats(
-                url=url,
-                response_status=response.status,
-                start_time=start_time,
-                end_time=end_time,
-                total_time=total_time,
-            )
-            return request_stats
+        async with session.get(url=url, timeout=30) as response:
+            await response.read()
     except TimeoutError:
         print(f"{url} timed out")
+        return
+    except ClientConnectorError:
+        print(f"invalid url {url}")
+        return
 
+    end_time_monotonic = time.monotonic()
+    total_time = end_time_monotonic - start_time_monotonic
+    status_code = response.status
+    request_stats = RequestInfo(
+        url=url,
+        status_code=status_code,
+        total_time=total_time,
+    )
+
+    return request_stats
 
 async def main(url_list) -> None:
-    async with aiohttp.ClientSession() as session:
+    connector = aiohttp.TCPConnector(limit=1000)
+    async with aiohttp.ClientSession(connector=connector) as session:
         tasks = []
+        results = []
         for c in url_list:
-            tasks.append(get(session=session, url=c))
+            tasks.append(get(session=session, url=c, ))
         for t in asyncio.as_completed(tasks):
             result = await t
-            print(result)
-        # results = await asyncio.gather(*tasks, return_exceptions=True)
+            if result:
+                line_printer(result)
+                results.append(result)
+
+        return results
+
+def line_printer(result: RequestInfo) -> None:
+    rounded_time = round(result.total_time, 4)
+    output_string = f"Request to {result.url} responded with " \
+                    f"{result.status_code} " \
+                    f"and took {rounded_time} seconds to complete"
+    print(output_string)
 
 
 if __name__ == '__main__':
     with open('url_list.txt') as f:
-        urls = f.read().splitlines()
-    url_list = urls
-    asyncio.run(main(url_list))
+        url_list = f.read().splitlines()
+    print(asyncio.run(main(url_list[:100])))
