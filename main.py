14a2d67a473bc9ab76edae57dca30071dfd51c39
Noah Skelton <skelton.noah@gmail.com>
2020-11-06 13:23:01 +0000

    Metrics class and printer added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+from statistics import mean, quantiles, median
+
 import aiohttp
 from asyncio.exceptions import TimeoutError
 from aiohttp.client_exceptions import (
@@ -26,11 +28,11 @@ class RequestInfo:
 async def get(
     session: aiohttp.ClientSession, url: str, timeout: int
 ) -> RequestInfo:
-    """
 
-    """
     start_time_monotonic = time.monotonic()
-    async with session.get(url=url, timeout=timeout, allow_redirects=False) as response:
+    async with session.get(
+        url=url, timeout=timeout, allow_redirects=False
+    ) as response:
         await response.read()
     end_time_monotonic = time.monotonic()
     total_time = end_time_monotonic - start_time_monotonic
@@ -46,7 +48,9 @@ async def get(
 
 async def main(url_list: List[str], timeout: int) -> List[RequestInfo]:
     connector = aiohttp.TCPConnector(limit=1000)
-    async with aiohttp.ClientSession(connector=connector, auto_decompress=False) as session:
+    async with aiohttp.ClientSession(
+        connector=connector, auto_decompress=False
+    ) as session:
         tasks = []
         results = []
         for url in url_list:
@@ -67,16 +71,50 @@ async def main(url_list: List[str], timeout: int) -> List[RequestInfo]:
 
 
 def line_printer(result: RequestInfo) -> None:
-    rounded_time = round(result.total_time, 4)
+    rounded_time_millis = round(result.total_time * 1000, 3)
     output_string = (
         f"Request to {result.url} responded with "
         f"{result.status_code} "
-        f"and took {rounded_time} seconds to complete"
+        f"and took {rounded_time_millis}ms to complete"
     )
     print(output_string)
 
 
+class Metrics:
+    """
+    Creates statistics based on response times of all requests
+    """
+
+    def __init__(self, request_info: List[RequestInfo]):
+        self.request_info = request_info
+        self.response_times = self._response_times()
+        self.mean = mean(self.response_times)
+        self.median = median(self.response_times)
+        self.ninetieth_percentile = quantiles(self.response_times, n=10)[-1]
+
+    def _response_times(self) -> List[float]:
+        response_times = []
+        for item in self.request_info:
+            response_times.append(item.total_time)
+        return response_times
+
+    def summary(self) -> None:
+        mean_millis = round(self.mean * 1000, 3)
+        median_millis = round(self.median * 1000, 3)
+        ninetieth_percentile_millis = round(
+            self.ninetieth_percentile * 1000, 3
+        )
+        output_summary = f"""
+        Mean response time = {mean_millis}ms
+        Median response time = {median_millis}ms
+        90th percentile of response times = {ninetieth_percentile_millis}ms
+        """
+        return output_summary
+
+
 if __name__ == "__main__":
-    p = Path('url_list.txt')
+    p = Path("url_list.txt")
     url_list = p.read_text().splitlines()
-    print(asyncio.run(main(url_list=url_list, timeout=10)))
+    request_info = asyncio.run(main(url_list=url_list, timeout=10))
+    metrics = Metrics(request_info=request_info)
+    print(metrics.summary())
